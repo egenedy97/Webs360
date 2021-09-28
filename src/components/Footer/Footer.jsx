7ed1e82a25ceb603e19e5c6382dafa2bafe1df66
Eslam Genedy <eslamgenedy377@yahoo.com>
2021-09-28 18:18:01 +0200

    Create a  Footer and the end of page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,62 @@
 import React from "react";
+import { StyledFooter, MainFooter, LeftText, LogoFooter, IconSpan, icons, TextCenter ,UList, RightText , StyleFont ,StrongFont } from './Style'
+import { Link, BrowserRouter as Router } from "react-router-dom";
+import { FaFacebookF, FaInstagram } from 'react-icons/fa';
 
-
-const Footer = ()=>{
+const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <Router>
+            <StyledFooter>
+                <MainFooter>
+                    <LeftText>
+                        <LogoFooter>
+                            Weds360
+                        </LogoFooter>
+                        <div>
+                            <IconSpan>
+
+                                <Link to="https://www.facebook.com/Weds360/">
+                                    <FaFacebookF style={icons} />
+                                </Link>
+                            </IconSpan>
+                            <IconSpan>
+
+                                <Link to="https://www.instagram.com/weds360/">
+                                    <FaInstagram style={icons} />
+                                </Link>
+
+                            </IconSpan>
+                        </div>
+                    </LeftText>
+                    <TextCenter>
+                        © Copyright
+                        <Link to="http://plus360.xyz/" style={{textDecoration:"none"}}>
+                            <StrongFont >
+                            &nbsp;  Plus360 &nbsp;
+                            </StrongFont>
+                             </Link>
+                        All Rights Reserved
+
+                    </TextCenter>
+                    <RightText>
+                        <UList>
+                        <Link to="https://weds360.com/pages/terms-conditions?locale=en" style={{textDecoration:"none"}} >
+                        <StyleFont>
+                        Terms & Conditions
+                        </StyleFont>
+                        </Link>
+                        <Link to = "https://weds360.com/pages/privacy-policy?locale=en" style={{textDecoration:"none"}}>
+                            <StyleFont>
+                            Privacy Policy
+                            </StyleFont>
+                        </Link>
+                        </UList>
+                    </RightText>
+
+                </MainFooter>
+            </StyledFooter>
+        </Router>
     )
 }
 
-export default Footer ; 
+export default Footer; 
--- src/components/Footer/Style.js
+++ src/components/Footer/Style.js
@@ -1,4 +1,93 @@
-import Styled from 'styled-components' 
+import Styled from 'styled-components'
+import { color } from '../../media/style'
+// import { device } from '../../media/style'
+export const StyledFooter = Styled.div`
+    background-color :${color.black} ; 
+    display : table; 
+    font-family:lato, sans-serif ; 
+    width :100% ; 
+    line-height:20px ; 
+    padding : 10px 100px 10px 15px ; 
+    color:white ; 
+    justify-content:space-between;
+    flex-direction:row; 
+    
+`
+
+export const MainFooter = Styled.div`
+background-color: ${color.black};
+width: 100%;
+display:table-row; 
 
 
-export const Footer = ``
+`
+
+export const LeftText = Styled.div`
+    display : inline-block 
+    color :${color.white};
+    width : 33% ;
+    display:table-cell; 
+    text-align:justify ;
+    
+
+`
+
+export const LogoFooter = Styled.h1`
+    font-weight : 500px ;
+    font-family:lato, sans-serif ; 
+    font-size : 20px ;    
+    color:inherit ; 
+`
+
+export const IconSpan = Styled.span`
+    margin-right :1% ;
+`
+
+export const icons = {
+    color: color.white
+}
+
+export const TextCenter = Styled.div`
+    display: inline-block ; 
+    width:33% ; 
+    font-size:10px ; 
+    font-weight: 100 ; 
+    color:${color.white} ; 
+    position:relative ; 
+    margin:auto ; 
+    text-align : center ; 
+    font-family:lato, sans-serif ; 
+    display:table-cell; 
+    vertical-align:bottom;
+`
+
+export const RightText = Styled.div`
+display: inline-block ; 
+width:33% ; 
+position:relative ; 
+display:table-cell;
+text-decoration:none ;
+vertical-align:bottom;
+`
+
+export const StyleFont = Styled.li`
+    color:${color.white};
+    text-align: right;
+    text-decoration:none ;
+    margin-top:0px ; 
+    flex-direction:column ;
+    font-size:10px ; 
+    font-weight: 100 ;
+    align-self: flex-end;
+`
+export const StrongFont = Styled.strong`
+    color:${color.lightBlue} ; 
+`
+
+export const UList = Styled.ul`
+    list-style:none ; 
+    flex-direction:column ;
+    marign-bottom :0px;
+
+
+`

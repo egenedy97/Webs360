90116452086bd9188405d2dafc75f278fae43636
Eslam Genedy <eslamgenedy377@yahoo.com>
2021-10-03 23:52:03 +0200

    create yield api

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,27 +1,81 @@
 import { call, takeEvery, put } from "redux-saga/effects";
 import axios from 'axios' ; 
 import {sagaActions} from '../actions/sagaActions' ; 
-import {fetchCategory} from '../reducers/Category' ; 
+import {fetchCategory ,numberPageCategory , searchCategoryAction} from '../reducers/Category' ; 
+import {fetchImagesPage , searchImages , fetchOnImage } from '../reducers/Images'
  
 let callAPI = async({url , method , data})=>{
     return await axios({
         method,  url,  data
     }).then(resp => {
         return resp;
+
     }).catch(error => {
         console.log(error);
     });  
 };
 
+export function* fetchPageImageData (action)
+{
+    try{
+        const result = yield call(async()=>{
+            return await callAPI({method:'GET' ,url:`http://localhost:4000/images?_page=${action.payload.page}&_limit=9&category_id=${action.payload.category_id}`  })
+          }); 
+        if(result)
+        {
+            yield put(fetchImagesPage({images:result.data , currentPage:action.payload.page}))
+        }
+    }catch(e)
+    {
+        yield put({type : 'FETCH_IMAGE_ERROR'});
+
+    }
+}
+
+export function* searchImage (action)
+{
+    try{
+        const result = yield call(async()=>{
+            return await callAPI({method:'GET' , url:`http://localhost:4000/images?name=${action.payload.data}`})
+        })
+        if(result)
+        {
+            yield put(searchImages({images:result.data}))
+        }
+    }catch(e)
+    {
+        yield put({type:'FETCH_IMAGE_ERROR'})
+    }
+}
+
+export function* fetchOneImage(action)
+{
+    try{
+        const result =  yield call(async()=>{
+            return await callAPI({method:'GET' , url:`http://localhost:4000/images?id=${action.payload.data}`})
+        }); 
+        const result2 =  yield call(async()=>{
+            return await callAPI({method:'GET' , url:`http://localhost:4000/images/_page=1&_limit=8&category_id=${action.payload.category_id}`})
+        });
+        if(result)
+        {
+            yield put(fetchOnImage({images:result.data , relatedImages :result2.data }))
+        }
+    }catch(e)
+    {
+        console.log(e);
+        yield put({type:'FETCH_IMAGE_ERROR'})
+    }
+}
 
 export function* fetchDataCategory (){
     try{
         const result = yield call(async()=>{
-          return await callAPI({method:'GET' ,url:'http://localhost:3004/category'  })
+          return await callAPI({method:'GET' ,url:`http://localhost:4000/category?_page=1&_limit=9`  })
         }); 
         if(result)
         {       
-            yield put(fetchCategory(result?.data))
+            yield put(fetchCategory({categorys:result?.data , currentPage:1 , totalpage:1}))
 
         }
     }catch(e)
@@ -30,17 +84,48 @@ export function* fetchDataCategory (){
     }
 }
 
-export function* fetchDataImages (){
+export function* fetchDataPageCategory (action){
     try{
-        
+        const result = yield call(async()=>{
+          return await callAPI({method:'GET' ,url:`http://localhost:4000/category?_page=${action.page}&_limit=9`  })
+        }); 
+        if(result)
+        {       
+            yield put(numberPageCategory({categorys:result?.data , currentPage:action.page}))
+
+        }
     }catch(e)
     {
-
+        yield put({type : 'FETCH_CATEGORY_ERROR'});
     }
-}
+};
 
+/* in the search you can find the value by write the full name   */
+
+export function* searchCategory (action){
+    try{
+        const result = yield call(async()=>{
+          return await callAPI({method:'GET' ,url:`http://localhost:4000/category?name=${action.payload.data}`  })
+        }); 
+        if(result)
+        {       
+            yield put(searchCategoryAction({categorys:result?.data }))
+
+        }
+    }catch(e)
+    {
+        console.log(e) ; 
+        yield put({type : 'FETCH_CATEGORY_ERROR'});
+    }
+};
 
 export default function* rootSaga()
 {
-    yield takeEvery(sagaActions.FETCH_CATEGORY , fetchDataCategory)
+    yield takeEvery(sagaActions.FETCH_INIT_CATEGORY , fetchDataCategory);
+    yield takeEvery(sagaActions.FETCH_PAGE_CATEGORY , fetchDataPageCategory);
+    yield takeEvery(sagaActions.SEARCH_CATEGORY , searchCategory);
+    yield takeEvery(sagaActions.FETCH_IMAGES_PAGE ,fetchPageImageData ) ; 
+    yield takeEvery(sagaActions.SEARCH_IMAGES , searchImage);
+    yield takeEvery(sagaActions.FETCH_ONE_IMAGE , fetchOneImage);
+
 };
